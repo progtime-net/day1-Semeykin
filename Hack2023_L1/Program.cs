4070416a6ab68a87696466abb9de3edda2c664ee
Mootfrost777 <andrey13102007@gmail.com>
2023-08-11 13:11:28 +0300

    Added greetings

--- Hack2023_L1/Program.cs
+++ Hack2023_L1/Program.cs
@@ -1,7 +1,11 @@
-﻿class Program
+﻿using System.Runtime.InteropServices;
+
+class Program
 {
     public static void Main(string[] args)
     {
-
+        Console.WriteLine("Hello, world");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello, " + name);
     }
 }
